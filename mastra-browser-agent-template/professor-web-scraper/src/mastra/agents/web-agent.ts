1a2f68255b0d5f990dcdf006838a3527411ea971
jdsmith435 <jdsmith435@comcast.net>
2025-08-16 13:32:46 -0600

    Updated agent instructions

--- mastra-browser-agent-template/professor-web-scraper/src/mastra/agents/web-agent.ts
+++ mastra-browser-agent-template/professor-web-scraper/src/mastra/agents/web-agent.ts
@@ -1,35 +1,44 @@
-import { openai } from '@ai-sdk/openai';
-import { Agent } from '@mastra/core/agent';
-import { Memory } from '@mastra/memory';
-import { pageActTool } from '../tools/page-act-tool';
-import { pageObserveTool } from '../tools/page-observe-tool';
-import { pageExtractTool } from '../tools/page-extract-tool';
-import { pageNavigateTool } from '../tools/page-navigate-tool';
+import { openai } from "@ai-sdk/openai";
+import { Agent } from "@mastra/core/agent";
+import { Memory } from "@mastra/memory";
+import { pageActTool } from "../tools/page-act-tool";
+import { pageObserveTool } from "../tools/page-observe-tool";
+import { pageExtractTool } from "../tools/page-extract-tool";
+import { pageNavigateTool } from "../tools/page-navigate-tool";
+import { TokenLimiter } from "@mastra/memory/processors";
 
-const memory = new Memory();
+const memory = new Memory({
+  processors: [new TokenLimiter(10000)],
+});
 
 export const webAgent = new Agent({
-  name: 'Web Assistant',
+  name: "Web Assistant",
   instructions: `
       You are a helpful web assistant that can navigate websites and extract information.
 
       Your primary functions are:
-      - Navigate to websites
-      - Observe elements on webpages
-      - Perform actions like clicking buttons or filling forms
-      - Extract data from webpages
+      - Navigate to University faculty directory
+      - Observe elements on on the directory page 
+      - Perform action like clicking on faculty names to navigate to their individual pages
+      - Extract the name, email, website, and photoUrl from webpages
+      - Iterate through all faculty pages
+      - Store the data in a database
 
       When responding:
-      - Ask for a specific URL if none is provided
-      - Be specific about what actions to perform
-      - When extracting data, be clear about what information you need
+      - Ask for a list of specific URLs if none are provided
 
       Use the pageActTool to perform actions on webpages.
       Use the pageObserveTool to find elements on webpages.
       Use the pageExtractTool to extract data from webpages.
       Use the pageNavigateTool to navigate to a URL.
 `,
-  model: openai('gpt-4o'),
+  model: openai("gpt-4o"),
   tools: { pageActTool, pageObserveTool, pageExtractTool, pageNavigateTool },
   memory: memory,
 });
+
+// removed instructions
+
+// When Responding
+// - Be specific about what actions to perform
+// - When extracting data, be clear about what information you need
